bc0fdf1858b6dbb485dacb33539db40d54454a2f
BlueAlskaren <joline.hallberg@outlook.com>
2023-03-10 17:31:09 +0100

    Added start/stop events

---
+++ src/general_classes/StartHändelse.java
@@ -0,0 +1,11 @@
+package general_classes;
+
+public class StartHändelse extends Event{
+    public StartHändelse(State state, EventQueue queue){
+        super(state, queue, 0);
+    }
+    @Override
+    public void runEvent(){
+        super.runEvent();
+    }
+}
