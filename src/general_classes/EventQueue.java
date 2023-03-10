ebea6d93d4389b34efbe646c03d473a60d15fbb4
isetnt2 <31891931+Isetnt2@users.noreply.github.com>
2023-03-10 15:42:07 +0100

    Added eventQueue

---
+++ src/general_classes/EventQueue.java
@@ -0,0 +1,22 @@
+package general_classes;
+
+import java.util.ArrayList;
+public class EventQueue {
+    ArrayList<Event> queue;
+
+    public EventQueue(){
+        this.queue = new ArrayList<Event>();
+    }
+
+    public void insert(Event e){
+        for (int i = 0; i < queue.size(); i++) {
+            if(e.getTime() < queue.get(i).getTime()){
+                queue.add(i, e);
+                continue;
+            }
+        }
+        queue.add(e);
+    }
+
+
+}
