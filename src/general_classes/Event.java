7741345c5285c6dfc67721fdd29e4a6d790f693a
BlueAlskaren <joline.hallberg@outlook.com>
2023-03-10 16:00:55 +0100

    yes

---
+++ src/general_classes/Event.java
@@ -0,0 +1,23 @@
+
+package general_classes;
+
+public class Event{
+    protected State state;
+    protected EventQueue queue;
+    protected double time;
+
+    public Event(State state, EventQueue queue, double time){
+        this.state = state;
+        this.queue = queue;
+        this.time = time;
+    }
+
+    public void runEvent(){
+        this.state.notify(this);
+    }
+
+    public double getTime(){
+        return time;
+    }
+}
+
