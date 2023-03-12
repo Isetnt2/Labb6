a9df43882841909b49a9b9c07ba798208435d296
isetnt2 <31891931+Isetnt2@users.noreply.github.com>
2023-03-12 18:03:37 +0100

    Added johns simulator kod

--- src/general_classes/Simulator.java
+++ src/general_classes/Simulator.java
@@ -2,14 +2,20 @@ package general_classes;
 public class Simulator{
     EventQueue queue = new EventQueue();
     State state = new State();
-    public void Simulator(){
 
+
+    public Simulator(EventQueue queue, State state){
+        this.queue = queue;
+        this.state = state;
     }
+
+    //körs sålänge kön inte är tom eller om stop i state returnerar true
     public void run(){
         while(queue.size() != 0){
             if(state.stop == true){
                 break;
             }
+            //kör igenom eventen i listan
             Event nyttEvent = queue.next();
             nyttEvent.runEvent();
         }
